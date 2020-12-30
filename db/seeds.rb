9e44773be964731ca7fe13fb5aacf09eb0e95c79
bebyx <bebyx@protonmail.com>
2020-12-31 00:33:53 +0200

    Add seeding of admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'bebyx',
+            email: 'artem@protonmail.com',
+            password: 'password',
+            admin: true)
+
+p "Created admin"
