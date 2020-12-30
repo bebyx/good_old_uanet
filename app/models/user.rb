4a619aa301a895f713ee3826061d1132dac3bdaa
bebyx <bebyx@protonmail.com>
2020-12-30 16:52:06 +0200

    Set minimum password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }
   has_secure_password
+  validates :password, length: { minimum: 6, maximum: 20 }, on: :create
 end
